c85d19aa99232877731ea4b039b9aba7ca032bdd
Effa Siddiqui <162863166+EffaSiddiqui@users.noreply.github.com>
2026-03-05 18:07:11 +0500

    script.js

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         const targetPage = link.getAttribute('data-page');
 
+        // Nav UI update karein
         navLinks.forEach(l => l.classList.remove('active-tab'));
         link.classList.add('active-tab');
 
+        // Pages switch karein
         pages.forEach(page => {
             page.classList.remove('current');
             if (page.id === targetPage) {
                 page.classList.add('current');
             }
         });
-        window.scrollTo(0, 0); // Mobile page switch par scroll up karega
     });
 });
